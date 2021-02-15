a011bca04fb67a8c097f56e21dcf25847d8bc8e3
Haruka <990628wjy@gmail.com>
2021-02-15 15:36:30 +0800

    update webget

--- apps/webget.cc
+++ apps/webget.cc
@@ -1,4 +1,4 @@
-#include "socket.hh"
+#include "tcp_sponge_socket.hh" 
 #include "util.hh"
 
 #include <cstdlib>
@@ -17,7 +17,7 @@ void get_URL(const string &host, const string &path) {
     // (not just one call to read() -- everything) until you reach
     // the "eof" (end of file).
 
-    TCPSocket sock;
+    CS144TCPSocket sock;
     sock.connect(Address(host, "http"));
     sock.write("GET " + path + " HTTP/1.1\r\n");
     sock.write("Host: " + host + "\r\n");
@@ -28,6 +28,8 @@ void get_URL(const string &host, const string &path) {
         auto rsp = sock.read();
         cout << rsp;
     }
+
+    socket.wait_until_closed();
     sock.close();
 }
 
